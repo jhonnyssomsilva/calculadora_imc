f133b0272ed0ddac5068a2f90c889e635a09ad77
Jhonnyssom <jhonnyssomux@gmail.com>
2023-12-13 14:32:53 -0300

    create `javascript`

---
+++ Assets/javascript/script.js
@@ -0,0 +1,32 @@
+//capturar o evento de submit do formulário
+const form = document.querySelector("#formulario");
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const inputPeso = e.target.querySelector("#peso");
+  const inputAltura = e.target.querySelector("#altura");
+
+  const peso = Number(inputPeso.value);
+  const altura = Number(inputAltura.value);
+
+  if (!peso) {
+    setResultado("Peso inválido", false);
+    return;
+  }
+  if (!altura) {
+    setResultado("Altura inválida", false);
+    return;
+  }
+});
+
+function criaP() {
+  const p = document.createElement("p");
+  return p;
+}
+
+function setResultado(msg, isValid) {
+  const resultado = document.querySelector("#resultado");
+  resultado.innerHTML = "";
+
+  const p = criaP;
+}
